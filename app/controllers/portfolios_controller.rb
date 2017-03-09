dba34418f91d1cc1144b01886cff4f2b2232d23c
Preston Matheson <pmphoto123@gmail.com>
2017-03-09 15:16:14 -0700

    Implemented Petergate functionality and fixed how a guest user functions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:show, :destroy, :edit, :update]
   layout "portfolio"
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
     def index
         @portfolio_items = Portfolio.all
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper
-    if current_user.is_a?(User) 
-     link_to "Log Out", destroy_user_session_path, method: :delete 
-    else 
+    if current_user.is_a?(GuestUser) 
      (link_to 'Login', new_user_session_path) +
      "<br>".html_safe +
-     (link_to 'Register', new_user_registration_path) 
+     (link_to 'Register', new_user_registration_path)  
+    else 
+     link_to "Log Out", destroy_user_session_path, method: :delete
     end  
   end
 
